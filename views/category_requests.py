00f514e2a54db89170015710659073839d992213
Grant Bell <grantjbell@gmail.com>
2022-04-21 13:35:01 -0500

    merge zachs branch

--- views/category_requests.py
+++ views/category_requests.py
@@ -6,7 +6,6 @@ def get_all_categories():
     # Open a connection to the database
     with sqlite3.connect("./db.sqlite3") as conn:
 
-        # Just use these. It's a Black Box.
         conn.row_factory = sqlite3.Row
         db_cursor = conn.cursor()
 
@@ -18,7 +17,7 @@ def get_all_categories():
         FROM Categories c 
         """)
 
-        # Initialize an empty list to hold all animal representations
+        # Initialize an empty list to hold all category representations
         categories = []
 
         # Convert rows of data into a Python list
@@ -27,10 +26,7 @@ def get_all_categories():
         # Iterate list of data returned from database
         for row in dataset:
 
-            # Create an animal instance from the current row.
-            # Note that the database fields are specified in
-            # exact order of the parameters defined in the
-            # category class above.
+            # Create an category instance from the current row.
             category = Category(row['id'], row['label'])
 
             
@@ -57,7 +53,8 @@ def get_single_category(id):
         # Load the single result into memory
         data = db_cursor.fetchone()
 
-        # Create an animal instance from the current row
+        # Create an category instance from the current row
         category = Category(data['id'], data['label'])
 
         return json.dumps(category.__dict__)  
+    
