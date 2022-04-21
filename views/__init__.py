22a6440c7f89765bce37441f5204229895dfb30a
Christian Haggerty <captivationstation@gmail.com>
2022-04-21 15:09:45 -0500

    demotion requests done

--- views/__init__.py
+++ views/__init__.py
@@ -1,19 +1,38 @@
 from .category_requests import get_all_categories, get_single_category, create_category
+
+
 from .post_requests import get_all_posts
 from .post_requests import get_single_post
 from .post_requests import create_post
 from .post_requests import delete_post
 from .post_requests import update_post
+
+
 from .user_requests import get_all_users
 from .user_requests import get_single_user
+
+
 from .reaction_requests import get_all_reactions
-from .tag_requests import get_all_tags
+
 
 from .comment_requests import get_all_comments
 from .comment_requests import get_single_comment
 from .comment_requests import delete_comment
 from .comment_requests import update_comment
 from .comment_requests import create_comment
+
+
 from .tag_requests import update_tag
 from .tag_requests import delete_tag
 from .tag_requests import create_tag
+from .tag_requests import get_all_tags
+
+from .subscription_requests import get_all_subscriptions
+from .subscription_requests import get_single_subscription
+from .subscription_requests import delete_subscription
+from .subscription_requests import create_subscription
+
+from .demotion_que_requests import get_all_demotion_queues
+from .demotion_que_requests import get_single_demotion_queue
+from .demotion_que_requests import delete_demotion_queue
+from .demotion_que_requests import create_demotion_queue
